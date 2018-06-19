af9018c962dba16d4f954f0a9e3772a773b73b17
BlunderingBeluga <40405783+BlunderingBeluga@users.noreply.github.com>
2018-06-19 13:35:08 -0700

    Setting up BearLibTerminal, implement walking @

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# roguelike-tutorial
+# Ruby Roguelike
+
+Here can be found my attempts to follow along with [/r/roguelikedev](https://www.reddit.com/r/roguelikedev/)'s 2018 dev-along for the [Complete Roguelike Tutorial](http://rogueliketutorials.com/libtcod/1). I'll be porting it to Ruby and [BearLibTerminal](http://foo.wyrd.name/en:bearlibterminal).
+
---
+++ main.rb
@@ -0,0 +1,41 @@
+require './lib/BearLibTerminal'
+
+class Game
+  def initialize
+    Terminal.open
+    Terminal.set("window.title = 'sample Ruby roguelike'")
+    @player_x = 40
+    @player_y = 12
+    @last_event = nil
+    
+    until @last_event == Terminal::TK_CLOSE
+      Terminal.clear
+      render
+      Terminal.refresh
+      @last_event = Terminal.read
+      update
+    end 
+  end
+  
+  def render
+    Terminal.print(@player_x, @player_y, '@')
+  end
+  
+  def update
+    case @last_event
+    when Terminal::TK_UP
+      @player_y -= 1
+    when Terminal::TK_DOWN
+      @player_y += 1
+    when Terminal::TK_LEFT
+      @player_x -= 1
+    when Terminal::TK_RIGHT
+      @player_x += 1
+    when Terminal::TK_CLOSE
+      Terminal.close
+      exit
+    end
+  end
+end
+
+Game.new
